311303617ff59bcc83fca1883ed5eaf1c1070aa0
Md Tauseef <tauseefk@Mds-MacBook-Pro.attlocal.net>
2021-08-28 18:38:26 -0700

    camera centered on player, glyphs

--- src/map.rs
+++ src/map.rs
@@ -22,18 +22,33 @@ impl Map {
     }
   }
 
-  pub fn render(&self, ctx: &mut BTerm) {
-    //iterating y first is faster with row-first striding due to memory cache usage
-    for y in 0..SCREEN_HEIGHT {
-      for x in 0..SCREEN_WIDTH {
-        let idx = map_idx(x, y);
+  pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+    ctx.set_active_console(0);
 
-        match self.tiles[idx] {
-          TileType::Floor => {
-            ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
-          }
-          TileType::Wall => {
-            ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+    //iterating y first is faster with row-first striding due to memory cache usage
+    for y in camera.top_y..camera.bottom_y {
+      for x in camera.left_x..camera.right_x {
+        if self.in_bounds(Point::new(x, y)) {
+          let idx = map_idx(x, y);
+          match self.tiles[idx] {
+            TileType::Floor => {
+              ctx.set(
+                x - camera.left_x,
+                y - camera.top_y,
+                YELLOW,
+                BLACK,
+                to_cp437('.'),
+              );
+            }
+            TileType::Wall => {
+              ctx.set(
+                x - camera.left_x,
+                y - camera.top_y,
+                GREEN,
+                BLACK,
+                to_cp437('#'),
+              );
+            }
           }
         }
       }
